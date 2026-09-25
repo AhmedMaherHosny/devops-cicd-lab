abe7bee291a9c38e44c064c32965708ecf24f07c
AhmedMaherHosny <ahmedmaher22292@gmail.com>
2026-09-25 04:05:29 +0300

    Listening to port 8080

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
 def add(a, b):
     return a + b
 
 
+class Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/health":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello from DevOps CI/CD Lab!")
+
+
 if __name__ == "__main__":
-    print(add(2, 3))
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+
+    print("Server listening on port 8080")
+
+    server.serve_forever()
